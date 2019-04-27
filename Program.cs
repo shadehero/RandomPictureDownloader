6ec0c6577d6f9edbf06f1d53e154a26b2831c0bf
Bartek Rybak <gotujcygana@gmail.com>
2019-04-27 02:19:54 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RandomPicture
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.Title = "Random Picture Downloader by Doctor Bogdan";
+
+			Console.WriteLine("Download Random Picture");
+
+			Console.Write("Set Width : ");
+			int width = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Set Height : ");
+			int height = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Starting Download Random Image");
+			string name = RandPic.Download("Images/", width, height);
+			Console.WriteLine("Download File {0} Done!", name);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ RandPic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using HtmlAgilityPack;
+using System.IO;
+
+namespace RandomPicture
+{
+	public class RandPic
+	{
+		static readonly string URL = "https://picsum.photos/";
+
+		public static string Download(string path, int width, int height)
+		{
+			string url = URL + width + "/" + height;
+			string randomFileName = Path.GetRandomFileName() + ".png";
+			using (WebClient client = new WebClient())
+			{
+				client.DownloadFile(url,path + randomFileName);
+			}
+
+			return randomFileName;
+		}
+	}
+}
